a9f9294489b0af84099be4af579d7defa92f5f43
barelysomethin <132691959+barelysomethin@users.noreply.github.com>
2024-07-04 11:08:31 -0700

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Countdown from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Countdown />
+    <Analytics />
   </React.StrictMode>,
 )
